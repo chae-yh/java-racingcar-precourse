788f80b84d6264c6b4f258e6201692afdb41bdbb
younghoon <bblnotbb@yahho.com>
2021-05-02 04:05:59 +0900

    feat : implement racing car class and its test code

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,6 +1,34 @@
 package racingcar;
 
+import java.util.regex.Pattern;
+
 public class RacingCar {
-	public RacingCar(String abcde) {
+	private static final int MIN_NAME_LENGTH = 0;
+	private static final int MAX_NAME_LENGTH = 5;
+	public static final String REGEX_TARGET = "^[0-9a-zA-Z]*$";
+	String name;
+
+	public RacingCar(String name) {
+		this.name = name;
+		isNameLengthInAllowedRange();
+		isNameConsistOfAllowedInput();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void isNameLengthInAllowedRange() {
+		if (this.name.length() <= MIN_NAME_LENGTH || this.name.length() > MAX_NAME_LENGTH) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void isNameConsistOfAllowedInput() {
+		Pattern pattern = Pattern.compile(REGEX_TARGET);
+
+		if (!pattern.matcher(this.name).find()) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,4 +1,38 @@
 package racingcar;
 
-public class RacingCar {
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+	@Test
+	@DisplayName("자동차 이름이 5자 이하인지 검증")
+	void is_name_lenght_shorter_than_5() {
+		assertThat(new RacingCar("abcde").getName().length() == 5).isTrue();
+
+		assertThatThrownBy(() -> {
+			new RacingCar("abcdef");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new RacingCar("");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("자동차 이름이 영어와 숫자로만 이루어졌는지 검증")
+	void is_name_consist_of_only_English_and_numbers() {
+		assertThatThrownBy(() -> {
+			new RacingCar("abc$");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new RacingCar("1bcㄱ");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new RacingCar("1b cd");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
 }
