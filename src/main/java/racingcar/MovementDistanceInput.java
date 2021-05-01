d5795d5e032ab2bd269e264bf402c76a5ccd3398
younghoon <bblnotbb@yahho.com>
2021-05-02 01:48:03 +0900

    refactor : rename field name and extract constant values

--- src/main/java/racingcar/MovementDistanceInput.java
+++ src/main/java/racingcar/MovementDistanceInput.java
@@ -1,6 +1,26 @@
 package racingcar;
 
 public class MovementDistanceInput {
-	public MovementDistanceInput(String s) {
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 100;
+	int input;
+
+	public MovementDistanceInput(String input) {
+		this.input = parseInputValue(input);
+		isInAllowedRange();
+	}
+
+	private int parseInputValue(String input) {
+		return Integer.parseInt(input);
+	}
+
+	private void isInAllowedRange() {
+		if (this.input <= MIN_VALUE || this.input >= MAX_VALUE) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public int getInput() {
+		return this.input;
 	}
 }
--- src/test/java/racingcar/MovementDistanceInputTest.java
+++ src/test/java/racingcar/MovementDistanceInputTest.java
@@ -1,4 +1,40 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MovementDistanceInputTest {
+	@Test
+	@DisplayName("입력 값이 숫자인지 체크함")
+	void is_number() {
+		assertThat(new MovementDistanceInput("11").getInput() == 11).isTrue();
+
+		assertThatThrownBy(() -> {
+			new MovementDistanceInput("abc");
+		}).isInstanceOf(NumberFormatException.class);
+	}
+
+	@Test
+	@DisplayName("입력 값이 0보다 크고 100보다 작은지 검증")
+	void is_inputvaluse_greater_than_0_and_lower_than_100() {
+		assertThat(new MovementDistanceInput("10").getInput() == 10).isTrue();
+
+		assertThatThrownBy(() -> {
+			new MovementDistanceInput("0");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new MovementDistanceInput("-1");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new MovementDistanceInput("100");
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			new MovementDistanceInput("123");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
 }
